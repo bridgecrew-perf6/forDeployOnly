2897f7cac820a3ef02e5d4d23c1b055985274bd8
Shubham_INT <shubham.shrivastava@indusnet.co.in>
2022-05-09 16:06:32 +0530

    msg1

--- finalSite/settings.py
+++ finalSite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ['your_app_name.herokuapp.com']
 
-ALLOWED_HOSTS = ['indusecommerce.herokuapp.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['ecommerceindus.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
